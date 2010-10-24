08f16ca6a9144a9bf774c5420402bbbd90d42d1e
chrishuttonch <chrishuttonch@gmail.com>
2010-10-24 11:28:40 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Momdod::Application.config.secret_token = 'f47a46c2e17a8bc4f3ae9585a4e6939d0f6f53831c88a1ce0ed8c614805f2d3e0d46d4abca43ce410246b714858376a6b60c073c7fc73c503248d4a87c1193bb'
